3dccb878a738ebb360d5899684c935085cb699d7
Shamil <shamilkadyrov@yandex.ru>
2022-10-06 00:43:26 +0300

    add Practical tasks Lesson1 DZ

---
+++ Practical tasks/Lesson1_DZ/DZ1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+int num_A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num_B = Convert.ToInt32(Console.ReadLine());
+
+if (num_A > num_B)
+{
+    Console.WriteLine("Первое число " + num_A + " больше чем второе " + num_B );    
+}
+
+if (num_B > num_A)
+{
+    Console.WriteLine("Первое число " + num_B + " больше чем второе " + num_A );    
+}
+            
+if (num_B == num_A)
+{
+    Console.WriteLine($"{num_B} и {num_A} равны");
+}
---
+++ Practical tasks/Lesson1_DZ/DZ4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0)
+{
+        Console.Write("Чётные числа от 1 до ");
+}
+
+int i = 2;
+
+while(i <= num)
+{
+    if (i%2 == 0)
+
+    {Console.Write(i + " ");}
+    i ++;
+}
+            
