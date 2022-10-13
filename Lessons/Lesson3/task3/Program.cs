0a70a97924f800ef590044b33933b42250e4d815
Shamil <shamilkadyrov@yandex.ru>
2022-10-14 00:36:57 +0300

    added lesson3 and lesson3 DZ

---
+++ Lessons/Lesson3/task3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D 
+// пространстве.
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+
+
+Console.Clear();
+//Math.Sqrt(); //вычисление квадратного корня
+
+const int x = 0;
+const int y = 1;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int [] InputPoint (int point)
+{
+    int[] answer = new int [2];
+    answer[x] = Prompt($"Введите x1 {point} -> ");
+    answer[y] = Prompt($"Введите y1 {point} -> ");
+    return answer;
+}
+int Power2 (int arg)
+{
+    return arg * arg;
+}
+int [] p1 = InputPoint(1);
+int [] p2 = InputPoint(2);
+
+double lenght = Math.Sqrt (Power2(p1[x] - p2[x])+Power2(p1[y] - p2[y])); // <- Math.Sqrt
+
+System.Console.WriteLine($"{lenght:f2}"); // f - количество знаков после запятой
---
+++ Lessons/Lesson3/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int N = Prompt("Введите число: ");
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(Math.Pow(i, 2));
+}
