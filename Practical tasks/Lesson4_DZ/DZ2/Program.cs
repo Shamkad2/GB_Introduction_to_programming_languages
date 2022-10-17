9da15cdf3dee0290b98cb87e1aa8c789e182f3f6
Shamil <shamilkadyrov@yandex.ru>
2022-10-17 23:43:52 +0300

    added lesson4 and lesson4 DZ

---
+++ Practical tasks/Lesson4_DZ/DZ2/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+// Функция должна на вход принимать число, а выдавать сумму его цифр
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
---
+++ Practical tasks/Lesson4_DZ/DZ3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 случайных элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 7, 8
+
+
+int lenArray = ReadInt("Введите любое число и получите массив из 8 случайных элементов: ");
+
+int[] randomArray = new int[8];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,29);
+    Console.Write(randomArray[i] + " ");
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
